947e8acde9d7f64365e71ddfe54bc2abcb2be686
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-06-05 14:41:34 +0700

    3. SearchBar & Logs Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
+import SearchBar from './components/layout/AppNavbar';
+import Logs from './components/logs/Logs';
+
 
 function App() {
   useEffect(() => {
@@ -9,9 +12,12 @@ function App() {
     M.AutoInit();
   })
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
