755b0e3db4f46e6c57ad9b0a212ea0345452da12
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-06-16 17:06:43 +0700

    11. Delete Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
 
 // export const getLogs = () => {
 //   return async (dispatch) => {
@@ -14,6 +14,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
 //   }
 // }
 
+// Get logs from server
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
@@ -31,6 +32,7 @@ export const getLogs = () => async dispatch => {
   }
 }
 
+// Add logs
 export const addLog = (log) => async dispatch => {
   try {
     setLoading();
@@ -45,6 +47,26 @@ export const addLog = (log) => async dispatch => {
     dispatch({
       type: ADD_LOG,
       payload: data
+    })
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    })
+  }
+}
+
+// Delete log from server
+export const deleteLog = (id) => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
     })
   } catch (err) {
     dispatch({
